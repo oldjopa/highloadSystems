4dc98ebc2a5f4819359c5099fa51aac8b91fdcce
larentina <118663828+larentiina@users.noreply.github.com>
2025-10-13 13:46:39 +0300

    add user integration test

---
+++ src/test/resources/sql/user/clear_test_users.sql
@@ -0,0 +1,5 @@
+DELETE FROM deal_status_history;
+DELETE FROM user_role;
+DELETE FROM deal;
+DELETE FROM aircraft;
+DELETE FROM app_user;
---
+++ src/test/resources/sql/user/post_assign_roles_after.sql
@@ -0,0 +1,1 @@
+UPDATE user_roles SET role_name = 'USER' WHERE user_id = 1;
