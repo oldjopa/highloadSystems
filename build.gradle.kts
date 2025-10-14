8f1a728494339daafcec5824a073b2c2f2ad3305
Ilya Ternovskiy <lolmaxlevel@gmail.com>
2025-10-14 19:50:31 +0300

    deps: removed unused jdbc dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
